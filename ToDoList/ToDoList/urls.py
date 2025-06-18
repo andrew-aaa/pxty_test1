b2bafb92a4dbd36948567f059b1c2f5f036ab0a2
Andrey Akulinin <akulinin-andrey2004@yandex.ru>
2025-06-18 19:38:10 +0300

    modified:   ToDoList/ToDoList/urls.py 	modified:   ToDoList/app/models.py

--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for ToDoList project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from app.views import TodoViewSet
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+router = DefaultRouter()
+router.register(r'app', TodoViewSet, basename='todo')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framewok.urls')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
--- ToDoList/app/models.py
+++ ToDoList/app/models.py
@@ -1,5 +1,36 @@
 from django.db import models
 from django.contrib.auth.models import User
-from 
+from django.utils import timezone
+
 
 # Create your models here.
+class Todo(models.Model):
+    PRIORITY = [
+        ('low', 'Низкий'),
+        ('medium', 'Средний'),
+        ('high', 'Высокий'),
+    ]
+
+    STATUS = [
+        ('done', 'Выполнено'),
+        ('not_done', 'Не выполнено'),
+    ]
+
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создание')
+    deadline = models.DateTimeField(verbose_name='Деадлайн')
+    priority = models.CharField(max_length=10, choices=PRIORITY, default='medium', verbose_name='Приоритет')
+    status = models.CharField(max_length=10, choices=STATUS, default='not_done', verbose_name='Статус')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos', verbose_name='Владелец')
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Задача'
+        verbose_name_plural = 'Задачи'
+
+    def __str__(self):
+        return self.title
+    
+    def is_overdue(self):
+        return timezone.now() > self.deadline
